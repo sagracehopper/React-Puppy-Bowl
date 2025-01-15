599a26ceaa0da6295c61623840454cf5bf7320e7
S A <sa@Ss-MacBook-Air.local>
2025-01-15 18:46:42 -0500

    PuppyBowl

--- Puppy Bowl/src/App.jsx
+++ Puppy Bowl/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { puppyList } from './data.js';
+import { useState } from 'react';
+import './App.css'; //imports the CSS file into your App.jsx
 
+
+
+//App component
 function App() {
-  const [count, setCount] = useState(0)
+  // Initialize puppies state with puppyList
+  const [puppies, setPuppies] = useState(puppyList);
+  //
+  const [featPupId, setFeatPupId] = useState(null);
+
+  //set the variable state. 
+
+  // Log the puppies array to verify data
+  console.log(puppies);
+  //returning for main app (function within a function)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
+
 
   return (
+    //fragments <>
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="logo"> 
+        <h1>Puppies </h1>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Puppy List</h1>
+      '
+      <ul>
+        {/* Use the puppies state to map and display the list */}
+        { 
+   puppies.map((puppy) => {
+     return <p onClick={()=>{ setFeatPupId(puppy.id)}}
+     key={puppy.id}>{puppy.name} </p>
+   }) //when someone clicks, the particular ID of that ONE puppy will show up with info 
+}
+      </ul>
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )} 
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
